020d69e5c9ed56ca1bd119b0f7692087befdfb6c
1412595 <trucvovttt@gmail.com>
2018-06-23 23:17:32 +0700

    Add data to database

--- DoAn/Database/BookStoreDB.sql
+++ DoAn/Database/BookStoreDB.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 -- Cơ sở dữ liệu: `bookstoredb`
 --
 
+create database bookstoredb;
+use bookstoredb;
 -- --------------------------------------------------------
 
 --
@@ -41,14 +43,14 @@ CREATE TABLE `account` (
 INSERT INTO `account` (`Account_ID`, `UserName`, `Account_Password`, `Account_Type`, `Account_Status`) VALUES
 (1, 'duynhat', '123456', 1, 1),
 (2, 'thanhtruc', '123456', 1, 1),
-(3, 'chithien2510', '123456', 2, 2),
-(4, 'anhthu1211', '123456', 2, 2),
-(5, 'queanh2126', '123456', 2, 2),
-(6, 'kimyen1124', '123456', 2, 2),
-(7, 'vantrung0012', '123456', 2, 2),
-(8, 'anhhieu5511', '123456', 2, 2),
-(9, 'huyenanh1155', '123456', 2, 2),
-(10, 'thuytien1910', '123456', 2, 2);
+(3, 'chithien', '123456', 2, 1),
+(4, 'anhthu', '123456', 2, 1),
+(5, 'queanh', '123456', 2, 1),
+(6, 'kimyen', '123456', 2, 1),
+(7, 'vantrung', '123456', 2, 1),
+(8, 'a', '123456', 2, 1),
+(9, 'b', '123456', 2, 1),
+(10, 'c', '123456', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -76,8 +78,50 @@ CREATE TABLE `book` (
 
 INSERT INTO `book` (`Book_ID`, `Book_Name`, `Author`, `Publisher`, `Publish_Date`, `Image`, `Price`, `Quantity`, `View_Number`, `Description`, `Kind_ID`) VALUES
 (1, 'High School English Grammar and Composition', 'Wren & Martin', 'Cambrigde University', '2010-05-15 00:00:00', 'HighSchoolEnglishGrammarandComposition.jpg', 15, 10, 100, 'It provides ample guidance and practice in sentence building, correct usage, comprehension, written composition and other allied areas so as to equip the student with the ability to communicate effectively in English.', 1),
-(2, 'The Woman in the Dunes', 'Kobo Abe', 'Tokyo', '1996-10-10 00:00:00', 'TheWomanintheDunes.jpg', 20, 20, 200, 'The Woman in the Dunes, by celebrated writer and thinker Kobo Abe, combines the essence of myth, suspense and the existential novel.\r\n \r\nAfter missing the last bus home following a day trip to the seashore, an amateur entomologist is offered lodging for the night at the bottom of a vast sand pit. But when he attempts to leave the next morning, he quickly discovers that the locals have other plans. Held captive with seemingly no chance of escape, he is tasked with shoveling back the ever-advancing sand dunes that threaten to destroy the village. His only companion is an odd young woman. Together their fates become intertwined as they work side by side at this Sisyphean task.', 2),
-(3, 'Easy French Step-by-Step', 'Myrna Bell Rochester', 'Paris', '2000-12-01 00:00:00', 'TheWomanintheDunes.jpg', 15, 20, 150, 'Easy French Step-by-Step proves that a solid grounding in grammar basics is the key to mastering a second language. You are quickly introduced to grammatical rules and concepts in order of importance, which you can build on as you progress through the book. You will also learn more than 300 verbs, chosen by their frequency of use. Numerous exercises and engaging readings help you quickly build your speaking and comprehension prowess.', 3);
+(2, 'Timesaver For Exams - IELTS', 'Norman Whitby, Julie Moore', 'NXB Tổng hợp TP.HCM', '1996-10-10 00:00:00', 'TheWomanintheDunes.jpg', 20, 20, 200, 'Chưa cập nhật', 1),
+(3, 'Bí Quyết Học Và Thi IELTS', 'Knowledge Link', 'NXB Phụ Nữ', '2000-12-01 00:00:00', 'TheWomanintheDunes.jpg', 15, 20, 150, 'Chưa cập nhật', 1),
+(4, 'Đàm Thoại Tiếng Nhật Hiện Đại (Kèm CD)', 'Ngọc Yến', 'NXB Thanh niên', '2000-12-01 00:00:00', 'DamThoaiTiengNhatHienDai.jpg', 20, 20, 200, 'Chưa cập nhật', 2),
+(5, 'Ngữ Pháp Tiếng Nhật Căn Bản (Tái Bản)', 'The Sakura', 'NXB Hồng Đức', '2010-05-15 00:00:00', 'NguPhapTiengNhatCanBan.jpg', 20, 20, 200, 'Chưa cập nhật', 2),
+(6, 'Tập Viết Tiếng Nhật Căn Bản Kanji', 'Mai Ngọc', 'NXB Đại học quốc gia Hà Nội', '2010-05-15 00:00:00', 'TapVietTiengNhatCanBanKanji.jpg', 20, 20, 200, 'Chưa cập nhật', 2),
+(7, 'Tập Viết Tiếng Hàn Dành Cho Người Mới Bắt Đầu', 'The Changmi', 'NXB Hồng Đức', '2010-05-15 00:00:00', 'TapVietTiengHanDanhChoNguoiMoiBatDau.jpg', 20, 20, 200, 'Chưa cập nhật', 3),
+(8, '3000 Từ Vựng Tiếng Hàn Theo Chủ Đề', 'Dương Thị Hồng Yên', 'NXB Hồng Đức', '2010-05-15 00:00:00', '3000TuVungTiengHanTheoChuDe.jpg', 20, 20, 200, 'Chưa cập nhật', 3),
+(9, 'Học Tiếng Hàn Thật Là Đơn Giản', 'Seung -eun Oh', 'NXB Đại học quốc gia Hà Nội', '2010-05-15 00:00:00', 'HocTiengHanThatLaDonGian.jpg', 20, 20, 200, 'Chưa cập nhật', 3),
+(10, 'Phát Triển Khách Hàng Tinh Gọn', 'Cindy Alvarez', 'NXB Công Thương', '2010-05-15 00:00:00', 'PhatTrienKhachHangTinhGon.jpg', 20, 20, 200, 'Chưa cập nhật', 4),
+(11, 'Kỹ Năng Bán Hàng (Tái Bản 2018)', 'William T. Brooks', 'Nxb Lao động - Xã hội', '2010-05-15 00:00:00', 'KyNangBanHang.jpg', 20, 20, 200, 'Chưa cập nhật', 4),
+(12, 'Chiến Lược Bán Hàng', 'Đàm Phương', 'NXB Thanh Hóa', '2010-05-15 00:00:00', 'ChienLuocBanHang.jpg', 20, 20, 200, 'Chưa cập nhật', 4),
+(13, 'Ngủ Ít Vẫn Khỏe', 'Satoru Tsubota', 'NXB Lao động', '2010-05-15 00:00:00', 'NguItVanKhoe.jpg', 20, 20, 200, 'Chưa cập nhật', 5),
+(14, 'Giấc Ngủ - Liều Thuốc Bổ Cho Cuộc Sống', 'Minh Tuấn, Khánh Hương', 'NXB Văn hóa Thông tin', '2010-05-15 00:00:00', 'GiacNgu-LieuThuocBoChoCuocSong.jpg', 20, 20, 200, 'Chưa cập nhật', 5),
+(15, '550 Bài Thuốc Đông Y Gia Truyền Trị Bách Bệnh', 'Quốc Trung', ' NXB Dân Trí', '2010-05-15 00:00:00', '550BaiThuocĐôngYGiaTruyenTriBachBenh.jpg', 20, 20, 200, 'Chưa cập nhật', 5),
+(16, 'Thích Là Nhích', 'Alex Tu', 'NXB Thế giới', '2010-05-15 00:00:00', 'ThichLaNhich.jpg', 20, 20, 200, 'Chưa cập nhật', 6),
+(17, 'Nước Đức Trong Lòng Bàn Tay', 'Trần Mai', 'NXB Thế giới', '2010-05-15 00:00:00', 'NuocDucTrongLongBanTay.jpg', 20, 20, 200, 'Chưa cập nhật', 6),
+(18, 'Hộ Chiếu Xanh Đi Quanh Thế Giới', 'Nhiều tác giả', 'NXB Thế giới', '2010-05-15 00:00:00', 'HoChieuXanhDiQuanhTheGioi.jpg', 20, 20, 200, 'Chưa cập nhật', 6),
+(19, 'Những Huyền Thoại Truyện Tranh Thế Giới - Thủy Thủ Popeye', 'E. C. Segar', 'NXB Kim Đồng', '2010-05-15 00:00:00', 'NhungHuyenThoaiTruyenTranhTheGioi-ThuyThuPopeye.jpg', 20, 20, 200, 'Chưa cập nhật', 7),
+(20, 'Truyện Tranh Khoa Học: Why? - Biển', 'Nhiều tác giả', 'NXB Trẻ', '2010-05-15 00:00:00', 'TruyenTranhKhoaHocWhy-Bien.jpg', 20, 20, 200, 'Chưa cập nhật', 7),
+(21, 'Truyện Tranh Từ Màn Ảnh - Vua Sư Tử', 'Disney', 'NXB Dân Trí', '2010-05-15 00:00:00', 'TruyenTranhTuManAnh-VuaSuTu.jpg', 20, 20, 200, 'Chưa cập nhật', 7),
+(22, 'Phút Im Lặng', 'Siegfriend Lenz', 'NXB Phụ Nữ', '2010-05-15 00:00:00', 'PhutImLang.jpg', 20, 20, 200, 'Chưa cập nhật', 8),
+(23, 'Bên Nhau Trọn Đời (Tái Bản 2018)', 'Cố Mạn', 'NXB Văn Học', '2010-05-15 00:00:00', 'BenNhauTronDoi.jpg', 20, 20, 200, 'Chưa cập nhật', 8),
+(24, 'Bức Thư Bị Lãng Quên (Tái Bản 2017)', 'Cố Tây Tước', 'NXB Thanh Niên', '2010-05-15 00:00:00', 'BucThuBiLangQuen.jpg', 20, 20, 200, 'Chưa cập nhật', 8),
+(25, 'Kỳ Án Ánh Trăng (Phiên Bản Mới)', 'Quỷ Cổ Nữ', 'NXB Văn Học', '2010-05-15 00:00:00', 'KyAnAnhTrang.jpg', 20, 20, 200, 'Chưa cập nhật', 9),
+(26, 'Thủy Xa Quán', 'Yukito Ayatsuji', 'NXB Hội Nhà Văn', '2010-05-15 00:00:00', 'ThuyXaQuan.jpg', 20, 20, 200, 'Chưa cập nhật', 9),
+(27, 'Sông Ngầm', 'Lôi Mễ', 'NXB Văn Học', '2010-05-15 00:00:00', 'SongNgam.jpg', 20, 20, 200, 'Chưa cập nhật', 9),
+(28, 'Nghĩ Đơn Giản, Sống Đơn Thuần', 'Tolly Burkan', 'NXB Thế Giới', '2010-05-15 00:00:00', 'NghiDonGianSongDonThuan.jpg', 20, 20, 200, 'Chưa cập nhật', 10),
+(29, 'Tuổi Trẻ Không Trì Hoãn', 'Thần Cách', 'NXB Thế Giới', '2010-05-15 00:00:00', 'TuoiTreKhongTriHoan.jpg', 20, 20, 200, 'Chưa cập nhật', 10),
+(30, 'Lật Đổ Ông Vua Trì Hoãn', 'Oopsy', 'NXB Thế Giới', '2010-05-15 00:00:00', 'LatDoOngVuaTriHoan.jpg', 20, 20, 200, 'Chưa cập nhật', 10),
+(31, 'Harvard Bốn Rưỡi Sáng', 'Xiu-ying Wei', 'NXB Thế Giới', '2010-05-15 00:00:00', 'HarvardBonRuoiSang.jpg', 20, 20, 200, 'Chưa cập nhật', 11),
+(32, 'Phất Tay Lung Lay Thế Giới', 'Oopsy', 'NXB Thế Giới', '2010-05-15 00:00:00', 'PhatTayLungLayTheGioi.jpg', 20, 20, 200, 'Chưa cập nhật', 11),
+(33, 'Cứ Đi Để Lối Thành Đường', 'Phoenix Ho', 'NXB Thế Giới', '2010-05-15 00:00:00', 'CuDiDeLoiThanhDuong.jpg', 20, 20, 200, 'Chưa cập nhật', 11),
+(34, 'Tuổi Trẻ Đáng Giá Bao Nhiêu', 'Rosie Nguyễn', 'NXB Hội Nhà Văn', '2010-05-15 00:00:00', 'TuoiTreDangGiaBaoNhieu.jpg', 20, 20, 200, 'Chưa cập nhật', 12),
+(35, 'Ngày Xưa Có Một Con Bò', 'Camilo Cruz', 'NXB Trẻ', '2010-05-15 00:00:00', 'NgayXuaCoMotConBo.jpg', 20, 20, 200, 'Chưa cập nhật', 12),
+(36, 'Hiểu Về Trái Tim', 'Minh Niệm', 'NXB Tổng Hợp TP.HCM', '2010-05-15 00:00:00', 'HieuVeTraiTim.jpg', 20, 20, 200, 'Chưa cập nhật', 12),
+(37, 'Tự Học VB.NET Và SQL SERVER 2008', 'KS. Hoàng Anh Quang', 'NXB Hồng Đức', '2010-05-15 00:00:00', 'TuHocVBNET.jpg', 20, 20, 200, 'Chưa cập nhật', 13),
+(38, 'Bài Tập Thực Hành Arduino - Lập Trình Điều Khiển Với Arduino', 'Phạm Quang Huy - Lê Cảnh Trung', 'NXB Khoa Học Và Kỹ Thuật', '2010-05-15 00:00:00', 'LapTrinhDieuKhienVoiArduino.jpg', 20, 20, 200, 'Chưa cập nhật', 13),
+(39, 'Vi Điều Khiển Và Ứng Dụng Arduino Dành Cho Người Tự Học', 'Phạm Quang Huy - Nguyễn Trọng Hiếu', 'NXB Bách Khoa Hà Nội', '2010-05-15 00:00:00', 'ViDieuKhienVaUngDungArduino.jpg', 20, 20, 200, 'Chưa cập nhật', 13),
+(40, 'Giáo Trình Thực Hành Microsoft Word Dùng Cho Các Phiên Bản', 'Phạm Quang Hiển - Phạm Phương Hoa', 'NXB Thanh Niên', '2010-05-15 00:00:00', 'ThucHanhMicrosoftWord.jpg', 20, 20, 200, 'Chưa cập nhật', 14),
+(41, 'Tự Học Microsoft Word Dành Cho Người Mới Bắt Đầu (Kèm CD)', 'VL - Comp', 'NXB Hồng Đức', '2010-05-15 00:00:00', 'TuHocMicrosoftWord.jpg', 20, 20, 200, 'Chưa cập nhật', 14),
+(42, 'Thực Hành Microsoft Word - Excel - Powerpoint 2016 Bằng Các Tuyệt Chiêu (Kèm CD)', 'Nhiều Tác Giả', 'NXB Đồng Nai', '2010-05-15 00:00:00', 'ThucHanhMSOffice.jpg', 20, 20, 200, 'Chưa cập nhật', 14),
+(43, 'Tự Học Thiết Kế Đồ Họa Trên Photoshop CS6', 'Nguyễn Đức Hiếu (biên soạn)', 'NXB Hồng Đức', '2010-05-15 00:00:00', 'TuHocThietKeDoHoa.jpg', 20, 20, 200, 'Chưa cập nhật', 15),
+(44, 'Thực Hành Autocad 2017 Cơ Bản Và Nâng Cao (Kèm CD)', 'VL - Comp', 'NXB Hồng Đức', '2010-05-15 00:00:00', 'ThucHanhAutocad.jpg', 20, 20, 200, 'Chưa cập nhật', 15),
+(45, 'Autocad Structural Detailing Dành Cho Người Bắt Đầu (Kết Cấu Thép)', 'Phạm Quang Hiển - Lê Thanh Nhật', 'NXB Khoa Học Và Kỹ Thuật', '2010-05-15 00:00:00', 'AutocadStructuralDetailing.jpg', 20, 20, 200, 'Chưa cập nhật', 15);
 
 -- --------------------------------------------------------
 
@@ -95,16 +139,12 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`Category_ID`, `Category_Name`) VALUES
-(9, 'Comics'),
-(10, 'Cookbooks'),
 (1, 'Foreign Language'),
-(4, 'Guide'),
-(6, 'Health'),
-(8, 'History'),
-(3, 'Horror'),
-(2, 'Mystery'),
-(7, 'Science'),
-(5, 'Travel');
+(2, 'Specialized'),
+(3, 'Story'),
+(4, 'Life skill'),
+(5, 'Informatics');
+
 
 -- --------------------------------------------------------
 
@@ -126,16 +166,14 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`Customer_ID`, `Customer_Name`, `Phone`, `Email`, `Gender`, `Account_ID`) VALUES
-(1, 'Nguyễn Duy Nhât', '01212073173', 'duynhat240896@gmail.com', b'1', 1),
-(2, 'Võ Thị Thanh Trúc', '01212014025', 'trucvo@gmail.com', b'0', 2),
-(3, 'Nguyễn Chí Thiện', '01212014235', 'chithien@gmail.com', b'1', 3),
-(4, 'Trần Thị Anh Thư', '01212259135', 'anhthu1211@gmail.com', b'0', 4),
-(5, 'Võ Quế Anh', '01212019562', 'queanh2126@gmail.com', b'0', 5),
-(6, 'Trần Thị Kim Yến', '01212014235', 'kimyen1124@gmail.com', b'0', 6),
-(7, 'Nguyễn Văn Trung', '01212014235', 'vantrung0012@gmail.com', b'1', 7),
-(8, 'Nguyễn Anh Hiếu', '01212014235', 'anhhieu5511@gmail.com', b'1', 8),
-(9, 'Trần Thị Huyền Anh', '01212014235', 'huyenanh1155@gmail.com', b'0', 9),
-(10, 'Vũ Thị Thủy Tiên', '01212014235', 'thuytien1910@gmail.com', b'0', 10);
+(1, 'Nguyễn Chí Thiện', '01212014235', 'chithien@gmail.com', b'1', 3),
+(2, 'Trần Thị Anh Thư', '01212259135', 'anhthu1211@gmail.com', b'0', 4),
+(3, 'Võ Quế Anh', '01212019562', 'queanh2126@gmail.com', b'0', 5),
+(4, 'Trần Thị Kim Yến', '01212014235', 'kimyen1124@gmail.com', b'0', 6),
+(5, 'Nguyễn Văn Trung', '01212014235', 'vantrung0012@gmail.com', b'1', 7),
+(6, 'Nguyễn A', '01212014235', 'a@gmail.com', b'1', 8),
+(7, 'Trần B', '01212259135', 'b@gmail.com', b'0', 9),
+(8, 'Võ C', '01212019562', 'c@gmail.com', b'0', 10);
 
 -- --------------------------------------------------------
 
@@ -166,6 +204,16 @@ CREATE TABLE `issuinghouse` (
 
 -- --------------------------------------------------------
 
+-- Đang đổ dữ liệu cho bảng `customer`
+--
+
+INSERT INTO `issuinghouse` (`IssuingHouse_ID`, `IssuingHouse_Name`, `Contact`, `Address`) VALUES
+(1, 'Alpha books', '01212017777', 'Chưa cập nhật'),
+(2, 'Văn Lang', '01212017776', 'Chưa cập nhật'),
+(3, 'Minh Tâm', '01212017775', 'Chưa cập nhật'),
+(4, 'NXB Phụ Nữ', '01212017774', 'Chưa cập nhật'),
+(5, 'Thái Hà', '01212017773', 'Chưa cập nhật');
+
 --
 -- Cấu trúc bảng cho bảng `kind`
 --
@@ -184,18 +232,20 @@ CREATE TABLE `kind` (
 INSERT INTO `kind` (`Kind_ID`, `Kind_Name`, `Description`, `Category_ID`) VALUES
 (1, 'English', 'The most popular language in the world', 1),
 (2, 'Japanese', 'The language of Japan', 1),
-(3, 'French', 'The language of France', 1),
-(4, 'Horror Comedy', 'Horror stories include humorous situations', 3),
-(5, 'Japanese horror', 'Horror stories by Japanese genre', 3),
-(6, 'Korean horror', 'Horror stories by Korean genre', 3),
-(7, 'Tourguide', 'Compile all the necessary instructions when traveling', 4),
-(8, 'Design guide', 'All design techniques on windows', 4),
-(9, 'Programming guide', 'Programming in different languages', 4),
-(10, 'Travel Asia', 'Asia travel guide', 5),
-(11, 'Travel Europe', 'Euro travel guide', 5),
-(12, 'Travel America', 'American travel guide', 5),
-(13, 'Infant', 'Infant health care', 6),
-(14, 'The Elderly', 'Health care for the elderly', 6);
+(3, 'Korean', 'The language of Korea', 1),
+(4, 'Economic', 'Economic', 2),
+(5, 'Medicine', 'Medicine', 2),
+(6, 'Travel', 'Travel', 2),
+(7, 'Comic', 'Comic', 3),
+(8, 'Love Story', 'Love Story', 3),
+(9, 'Ghost Story', 'Ghost Story', 3),
+(10, 'Beauty Life', 'Beauty Life', 4),
+(11, 'Orientation', 'Orientation', 4),
+(12, 'Living Thinking', 'Living Thinking', 4),
+(13, 'Developing', 'Developing', 5),
+(14, 'Office', 'Office', 5),
+(15, 'Design', 'Design', 5);
+
 
 -- --------------------------------------------------------
 
